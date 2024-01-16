cd263593869b48611776983f564433b3cbf6234d
Jorgen Syvertsen <jorgesyv@stud.ntnu.no>
2024-01-16 19:31:31 +0100

    feat(HLC): Add FollowTheRedDot
    Adapting the 2D game FollowTheRedDot to work in the 3D project on the
    HoloLens.

---
+++ HoloLensClient/Assets/Scripts/Exact/NumberTextPro.cs
@@ -0,0 +1,29 @@
+using TMPro;
+using UnityEngine;
+
+namespace Svanesjo.MRIoT.Exact
+{
+    [RequireComponent(typeof(TextMeshProUGUI))]
+    public class NumberTextPro : MonoBehaviour
+    {
+        private TextMeshProUGUI _textMeshPro;
+
+        private void Awake()
+        {
+            _textMeshPro = GetComponent<TextMeshProUGUI>();
+            _textMeshPro.text = "";
+        }
+
+        public void SetNumber(int number)
+        {
+            if (number < 0)
+            {
+                _textMeshPro.text = "";
+            }
+            else
+            {
+                _textMeshPro.text = number.ToString();
+            }
+        }
+    }
+}
