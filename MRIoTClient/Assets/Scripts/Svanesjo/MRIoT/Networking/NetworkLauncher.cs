0715f112520411ed2f3d0688d183945c93a9bb28
Jorgen Syvertsen <jorgesyv@stud.ntnu.no>
2024-02-02 13:14:55 +0100

    feat: Periodically check for lost connection
    - Client periodically checks if lost connection to server
        - If no connection, recheck after 30 seconds and if still no
          connection, restart client
    - Refactor NetworkLauncher to be component on NetworkManager, rather
      than a child GameObject

--- MRIoTClient/Assets/Scripts/Svanesjo/MRIoT/Networking/NetworkLauncher.cs
+++ MRIoTClient/Assets/Scripts/Svanesjo/MRIoT/Networking/NetworkLauncher.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using NaughtyAttributes;
 using Unity.Netcode;
@@ -16,6 +17,7 @@ namespace Svanesjo.MRIoT.Networking
         Client
     }
 
+    [RequireComponent(typeof(NetworkManager))]
     public class NetworkLauncher : MonoBehaviour
     {
         [Required, SerializeField] private Material serverMaterial = null!;
@@ -27,27 +29,38 @@ namespace Svanesjo.MRIoT.Networking
 
         [Required, SerializeField] private MeshRenderer floor = null!;
 
+        [SerializeField] private int checkConnectionInterval = 10;
+        [SerializeField] private int reinitializeIfConnectionLostFor = 30;
+
+        private NetworkManager _networkManager = null!;
+
         private void Start()
         {
-            if (serverMaterial is null || hostMaterial is null || clientMaterial is null || floor is null)
-            {
-                throw new Exception("Required fields are null");
-            }
+            _networkManager = GetComponent<NetworkManager>();
+            if (_networkManager == null)
+                throw new MissingComponentException();
 
+            if (serverMaterial is null || hostMaterial is null || clientMaterial is null || floor is null)
+                throw new Exception("Required fields are null");
+
+            Initialize();
+        }
+
+        private void Initialize()
+        {
             Debug.Log("NetworkLauncher starting...");
             StartAs(Application.isEditor ? editorStartType : deviceStartType);
         }
 
         private void StartAs(StartType startType)
         {
-            NetworkManager networkManager = GetComponentInParent<NetworkManager>();
             switch (startType)
             {
                 case StartType.Server:
                 {
                     Debug.Log("Starting Server...");
                     floor.SetMaterials(new List<Material> { serverMaterial });
-                    var state = networkManager.StartServer();
+                    var state = _networkManager.StartServer();
                     Debug.LogWarning($"NetworkLauncher Started: {state}");
                     break;
                 }
@@ -55,7 +68,7 @@ namespace Svanesjo.MRIoT.Networking
                 {
                     Debug.Log("Starting Host...");
                     floor.SetMaterials(new List<Material> { hostMaterial });
-                    var state = networkManager.StartHost();
+                    var state = _networkManager.StartHost();
                     Debug.LogWarning($"NetworkLauncher Started: {state}");
                     break;
                 }
@@ -63,8 +76,9 @@ namespace Svanesjo.MRIoT.Networking
                 {
                     Debug.Log("Starting Client...");
                     floor.SetMaterials(new List<Material> { clientMaterial });
-                    var state = networkManager.StartClient();
-                    Debug.LogWarning($"NetworkLauncher  Started: {state}");
+                    var state = _networkManager.StartClient();
+                    Debug.LogWarning($"NetworkLauncher Started: {state}");
+                    StartCoroutine(CheckConnection());
                     break;
                 }
             }
@@ -72,9 +86,45 @@ namespace Svanesjo.MRIoT.Networking
 
         private void OnApplicationPause(bool pauseStatus)
         {
-            Debug.Log($"NetworkLauncher onApplicationPause({pauseStatus})");
-            if (pauseStatus) return;
-            Start();
+            var isServer = _networkManager.IsServer;
+            var connected = _networkManager.IsConnectedClient;
+            Debug.Log($"NetworkLauncher onApplicationPause paused: {pauseStatus}, connected: {connected}, isServer: {isServer}");
+            if (pauseStatus || isServer || connected)
+                return;
+
+            Debug.Log("NetworkLauncher onApplicationPause rerunning Initialize");
+            Initialize();
+        }
+
+        private IEnumerator CheckConnection()
+        {
+            Debug.Log($"NetworkLauncher checkConnection-coroutine running in background, checking every {checkConnectionInterval} seconds");
+            while (true)
+            {
+                if (_networkManager.IsServer)
+                {
+                    Debug.LogError("NetworkLauncher checkConnection running on server, which should never happen");
+                    break;
+                }
+
+                var connected = _networkManager.IsConnectedClient;
+                Debug.Log($"NetworkLauncher checkConnection connected: {connected}, rechecking in {checkConnectionInterval} seconds");
+                if (!connected)
+                {
+                    Debug.Log($"NetworkLauncher checkConnection not connected, rechecking in {reinitializeIfConnectionLostFor} seconds before rerunning Initialize");
+                    yield return new WaitForSeconds(reinitializeIfConnectionLostFor);
+
+                    connected = _networkManager.IsConnectedClient;
+                    if (!connected)
+                    {
+                        Debug.Log("NetworkLauncher checkConnection still not connected, rerunning Initialize");
+                        Initialize();
+                        break;
+                    }
+                }
+
+                yield return new WaitForSeconds(checkConnectionInterval);
+            }
         }
     }
 }
